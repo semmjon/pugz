e22a3d0b84545bf8bfcd2a57204e4d2d34fe529c
Maël Kerbiriou <mael.kerbiriou@inria.fr>
2018-05-04 13:54:18 +0200

    Split deflate_decompress.cpp in multiple files

---
+++ lib/deflate_window.hpp
@@ -0,0 +1,345 @@
+#ifndef DEFLATE_WINDOW_HPP
+#define DEFLATE_WINDOW_HPP
+
+#include <cstdint>
+#include <algorithm>
+
+#include "common_defs.h"
+#include "assert.hpp"
+#include "unaligned.h"
+#include "input_stream.hpp"
+
+#define PRINT_DEBUG(...) {}
+//#define PRINT_DEBUG(...) {fprintf(stderr, __VA_ARGS__);}
+#define DEBUG_FIRST_BLOCK(x) {}
+//#define DEBUG_FIRST_BLOCK(x) {x}
+
+/**
+ * @brief A window of the size of one decoded shard (some deflate blocks) plus it's 32K context
+ */
+template<unsigned _buffer_bits=21, unsigned _context_bits=15>
+struct DeflateWindow {
+    //TODO: should we set these at runtime ? context_bits=15 should be fine for all compression levels
+    // buffer_size could be adjusted on L3 cache size.
+    // But: there is a runtime cost as we loose some optimizations
+    static constexpr unsigned buffer_bits = _buffer_bits;
+    static constexpr unsigned context_bits = _context_bits;
+    static_assert(context_bits < buffer_bits, "Buffer too small for context");
+
+    using wsize_t = uint_fast32_t; /// Type for positive offset in buffer
+    using wssize_t = int_fast32_t; /// Type for signed offsets in buffer
+    static_assert(std::numeric_limits<wsize_t>::max() > (1ULL << buffer_bits), "Buffer too large for wsize_t");
+    static_assert(std::numeric_limits<wssize_t>::max() > (1ULL << buffer_bits), "Buffer too large for wssize_t");
+
+    static constexpr wsize_t context_size = wsize_t(1)<<context_bits;
+    static constexpr wsize_t buffer_size = wsize_t(1)<<buffer_bits;
+
+    DeflateWindow() :
+        buffer(new byte[buffer_size]),
+        buffer_end(buffer + buffer_size),
+        next(buffer)
+    {}
+
+    ~DeflateWindow()    {
+        if(next != nullptr)
+            delete[] buffer;
+    }
+
+    DeflateWindow(DeflateWindow&& from) :
+        buffer(from.buffer),
+        buffer_end(buffer + buffer_size),
+        next(from.next)
+    {
+        from.next = nullptr;
+    }
+
+    /// Clone the context window
+    DeflateWindow(const DeflateWindow& from) : DeflateWindow()
+    { clone_context(from); }
+
+    /// Clone the context window
+    DeflateWindow& operator=(const DeflateWindow& from) {
+        clear();
+        clone_context(from);
+    }
+
+    void clear(size_t begin=0) {
+        next = buffer + begin;
+    }
+
+    wsize_t size() const
+    {
+        assert(next >= buffer);
+        return next - buffer;
+    }
+
+    wsize_t available() const
+    {
+        assert(buffer_end >= next);
+        return buffer_end - next;
+    }
+
+    bool push(byte c) {
+        assert(available() >= 1); // Enforced by do_block
+        *next++ = c;
+        return true;
+    }
+
+    /* return true if it's a reasonable offset, otherwise false */
+    bool copy_match(wsize_t length, wsize_t offset) {
+        /* The match source must not begin before the beginning of the
+         * output buffer.  */
+        assert(offset <= size());
+        assert(available() >= length);
+
+        if (length <= (3 * WORDBYTES) &&
+            offset >= WORDBYTES &&
+            length + (3 * WORDBYTES) <= available())
+        {
+            /* Fast case: short length, no overlaps if we copy one
+             * word at a time, and we aren't getting too close to
+             * the end of the output array.  */
+            copy_word_unaligned(next - offset + (0 * WORDBYTES),
+                                next + (0 * WORDBYTES));
+            copy_word_unaligned(next - offset + (1 * WORDBYTES),
+                                next + (1 * WORDBYTES));
+            copy_word_unaligned(next - offset + (2 * WORDBYTES),
+                                next + (2 * WORDBYTES));
+        } else {
+            const byte *src = next - offset;
+            byte *dst = next;
+            const  byte* const dst_end = dst + length;
+
+            if (likely(buffer_end - dst_end >= WORDBYTES - 1)) {
+                if (offset >= WORDBYTES) {
+                    copy_word_unaligned(src, dst);
+                    src += WORDBYTES;
+                    dst += WORDBYTES;
+                    if (dst < dst_end) {
+                        do {
+                            copy_word_unaligned(src, dst);
+                            src += WORDBYTES;
+                            dst += WORDBYTES;
+                        } while (dst < dst_end);
+                    }
+                } else if (offset == 1) {
+                    machine_word_t v = repeat_byte(*(dst - 1));
+                    do {
+                        store_word_unaligned(v, dst);
+                        src += WORDBYTES;
+                        dst += WORDBYTES;
+                    } while (dst < dst_end);
+                } else {
+                    *dst++ = *src++;
+                    *dst++ = *src++;
+                    do {
+                        *dst++ = *src++;
+                    } while (dst < dst_end);
+                }
+            } else {
+                *dst++ = *src++;
+                *dst++ = *src++;
+                do {
+                      *dst++ = *src++;
+                } while (dst < dst_end);
+            }
+        }
+
+        DEBUG_FIRST_BLOCK(fprintf(stderr,"match of length %d offset %d: ",length,offset);)
+        DEBUG_FIRST_BLOCK( for (wsize_t int i = 0; i < length; i++) fprintf(stderr,"%c",buffer[next+i-buffer]); fprintf(stderr,"\n");)
+
+        next += length;
+
+        return true;
+    }
+
+    bool copy(InputStream& in, wsize_t length) {
+        assert(available() >= length);
+        in.copy(next, length);
+        next += length;
+        return true;
+    }
+
+    /// Move the 32K context to the start of the buffer
+    size_t flush(size_t keep_size=context_size) {
+        assert(size() > keep_size);
+        assert(buffer + keep_size <= next - keep_size); // src and dst aren't overlapping
+        memcpy(buffer, next - keep_size, keep_size);
+        size_t moved_by = size() - keep_size;
+
+        // update next pointer
+        next = buffer + keep_size;
+
+        return moved_by;
+    }
+
+    bool notify_end_block(InputStream& in_stream) const { return true; }
+
+protected:
+    void clone_context(const DeflateWindow& from) {
+        assert(next == buffer); // Empty state: call clear() before.
+        assert(from.size() >= context_size);
+        memcpy(buffer, from.next-context_size, context_size);
+        next += context_size;
+    }
+
+    static forceinline machine_word_t
+    repeat_byte(byte b)
+    {
+        machine_word_t v = static_cast<machine_word_t>(b);
+
+        STATIC_ASSERT(WORDBITS == 32 || WORDBITS == 64);
+
+        v |= v << 8;
+        v |= v << 16;
+        v |= v << ((WORDBITS == 64) ? 32 : 0);
+        return v;
+    }
+
+    static forceinline void
+    copy_word_unaligned(const void *src, void *dst)
+    {
+        store_word_unaligned(load_word_unaligned(src), dst);
+    }
+
+
+
+    byte* const buffer; /// Allocated output buffer
+    const byte* const buffer_end; /// Past the end pointer
+    byte* next; /// Next byte to be written
+};
+
+
+class FlushableDeflateWindow : public DeflateWindow<> {
+    using Base = DeflateWindow;
+
+public:
+    FlushableDeflateWindow(byte* _target, byte* _target_end)
+        : DeflateWindow(),
+        target(_target), target_start(_target), target_end(_target_end)
+    {}
+
+    wsize_t flush(wsize_t start=0, wsize_t window_size=context_size) {
+        assert(size() >= window_size);
+        size_t evict_size = size() - window_size - start;
+        assert(buffer + start + evict_size == next - window_size);
+        assert(target + evict_size < target_end);
+
+        memcpy(target, buffer + start, evict_size);
+        target += evict_size;
+
+        // Then move the context to the start
+        return DeflateWindow::flush(window_size);
+    }
+
+    wsize_t get_evicted_length() {
+        assert(target >= target_start);
+        return wsize_t(target - target_start);
+    }
+
+protected:
+    byte* target;
+    const byte* target_start;
+    const byte* target_end;
+};
+
+
+template<typename _Base=DeflateWindow<>, byte _unknown_fill=byte('?')>
+struct SyncingDeflateWindow : public _Base {
+    using Base = _Base;
+    using typename Base::wsize_t;
+    using Base::context_size;
+    using Base::buffer;
+    using Base::size;
+
+    static constexpr byte unknown_fill = _unknown_fill;
+
+    SyncingDeflateWindow() : Base()
+    {
+        memset(buffer, int(unknown_fill), context_size);
+        clear();
+    }
+
+    void clear(wsize_t begin=0) {
+//        assert(std::all_of(buffer, buffer + context_size,
+//                           [](const byte& b){ return b == unknown_fill; }));
+        Base::clear(begin+context_size);
+    }
+
+    bool push(byte c) {
+        if( c > byte('~') || c < byte('\t')) {
+            PRINT_DEBUG("fail, unprintable literal unexpected in fastq\n");
+            return false;
+        }
+
+        return Base::push(c);
+    }
+
+    wsize_t flush(size_t=0) {
+        assert(false); //FIXME: Could this still happen ? Comment this if it's a acceptable sync faillure
+        return 0;
+    }
+
+    bool copy(InputStream& in, wsize_t length) {
+        if(in.check_ascii(length)) {
+            return Base::copy(in, length);
+        }
+        PRINT_DEBUG("fail, unprintable uncompressed segment unexpected in fastq\n");
+        return false;
+    }
+
+    bool copy_match(wsize_t length, wsize_t offset) {
+        /* The match source must not begin before the beginning of the
+         * output buffer.  */
+        if (offset > context_size || offset > size() )
+        {
+            PRINT_DEBUG("fail, copy_match, offset %d (window size %d)\n",(int)offset, size());
+            return false;
+        }
+
+        assert(offset <= size()); // since size() >= context_size
+        assert(offset != 0); // Could not happen with the way offset are encoded
+
+        return Base::copy_match(length, offset);
+    }
+};
+
+/// Keeps a pointer last_processed limiting the flush
+/// Also keeps track of the current_blk, allowing multithread synchronization
+struct StreamingDeflateWindow : public DeflateWindow<> {
+    StreamingDeflateWindow() : DeflateWindow() {
+        clear();
+    }
+
+    void clear(size_t begin=0) {
+        DeflateWindow::clear(begin);
+        last_processed = next;
+        current_blk = next;
+    }
+
+    size_t flush(size_t& keep_size) {
+        assert(next >= last_processed);
+        keep_size = std::max(keep_size, size_t(next-last_processed));
+        size_t moved_by = DeflateWindow::flush(keep_size);
+
+        current_blk -= moved_by;
+        last_processed -= moved_by;
+        assert(last_processed >= buffer);
+
+        return moved_by;
+    }
+
+    void notify_end_block() {
+        current_blk = next;
+    }
+
+    ssize_t last_processed_pos_in_block() {
+        return last_processed - current_blk;
+    }
+
+protected:
+    byte* last_processed;
+    byte* current_blk;
+};
+
+#endif // DEFLATE_WINDOW_HPP
---
+++ lib/input_stream.hpp
@@ -0,0 +1,273 @@
+#ifndef INPUT_STREAM_HPP
+#define INPUT_STREAM_HPP
+
+#include <cstring> // memcpy
+
+#include "common_defs.h"
+#include "assert.hpp"
+#include "unaligned.h"
+
+/**
+ * @brief Model an compressed gzip input stream
+ * It can be read by dequeing n<32 bits at a time or as byte aligned u16 words
+
+ * The state of the "input bitstream" consists of the following variables:
+ *
+ *	- in_next: pointer to the next unread byte in the input buffer
+ *
+ *	- in_end: pointer just past the end of the input buffer
+ *
+ *	- bitbuf: a word-sized variable containing bits that have been read from
+ *		  the input buffer.  The buffered bits are right-aligned
+ *		  (they're the low-order bits).
+ *
+ *	- bitsleft: number of bits in 'bitbuf' that are valid.
+ *
+ */
+class InputStream {
+public: //protected: //FIXME
+    /*
+     * The type for the bitbuffer variable ('bitbuf' described above).  For best
+     * performance, this should have size equal to a machine word.
+     *
+     * 64-bit platforms have a significant advantage: they get a bigger bitbuffer
+     * which they have to fill less often.
+     */
+    using bitbuf_t = machine_word_t;
+    using bitbuf_size_t = uint_fast32_t;
+
+    /** State */
+    const byte *const begin;
+    const byte *restrict in_next; /// Read pointer
+    const byte *restrict const in_end; /// Adress of the byte after input
+    bitbuf_t bitbuf = bitbuf_t(0); /// Bit buffer
+    bitbuf_size_t bitsleft = 0; /// Number of valid bits in the bit buffer
+    bitbuf_size_t overrun_count = 0;
+    bool reached_final_block = false;
+
+    /**
+     * Number of bits the bitbuffer variable can hold.
+     */
+    static constexpr bitbuf_size_t bitbuf_length = 8 * sizeof(bitbuf_t);
+
+    /**
+     * The maximum number of bits that can be requested to be in the bitbuffer
+     * variable.  This is the maximum value of 'n' that can be passed
+     * ensure_bits(n).
+     *
+     * This not equal to BITBUF_NBITS because we never read less than one byte at a
+     * time.  If the bitbuffer variable contains more than (BITBUF_NBITS - 8) bits,
+     * then we can't read another byte without first consuming some bits.  So the
+     * maximum count we can ensure is (BITBUF_NBITS - 7).
+     */
+    static constexpr bitbuf_size_t bitbuf_max_ensure = bitbuf_length - 7;
+
+    /**
+     * Does the bitbuffer variable currently contain at least 'n' bits?
+     */
+    inline bool have_bits(size_t n) const
+    { return bitsleft >= n; }
+
+    /**
+     * Fill the bitbuffer variable by reading the next word from the input buffer.
+     * This can be significantly faster than fill_bits_bytewise().  However, for
+     * this to work correctly, the word must be interpreted in little-endian format.
+     * In addition, the memory access may be unaligned.  Therefore, this method is
+     * most efficient on little-endian architectures that support fast unaligned
+     * access, such as x86 and x86_64.
+     */
+    inline void fill_bits_wordwise() {
+        bitbuf |= get_unaligned_leword(in_next) << bitsleft;
+        in_next += (bitbuf_length - bitsleft) >> 3;
+        bitsleft += (bitbuf_length - bitsleft) & ~7;
+    }
+
+    /**
+     * Fill the bitbuffer variable, reading one byte at a time.
+     *
+     * Note: if we would overrun the input buffer, we just don't read anything,
+     * leaving the bits as 0 but marking them as filled.  This makes the
+     * implementation simpler because this removes the need to distinguish between
+     * "real" overruns and overruns that occur because of our own lookahead during
+     * Huffman decoding.  The disadvantage is that a "real" overrun can go
+     * undetected, and libdeflate_deflate_decompress() may return a success status
+     * rather than the expected failure status if one occurs.  However, this is
+     * irrelevant because even if this specific case were to be handled "correctly",
+     * one could easily come up with a different case where the compressed data
+     * would be corrupted in such a way that fully retains its validity.  Users
+     * should run a checksum against the uncompressed data if they wish to detect
+     * corruptions.
+     */
+    inline void fill_bits_bytewise() {
+        do {
+               if (likely(in_next != in_end))
+                       bitbuf |= bitbuf_t(*in_next++) << bitsleft;
+               else
+                       overrun_count++;
+               bitsleft += 8;
+        } while (bitsleft <= bitbuf_length - 8);
+    }
+
+public:
+    InputStream(const byte* in, size_t len) :
+        begin(in), in_next(in), in_end(in + len)
+    {}
+
+    InputStream(const InputStream&) = default;
+
+    /// states asignments for the same stream (for backtracking)
+    InputStream& operator=(const InputStream& from) {
+        assert(begin == from.begin && in_end == from.in_end);
+
+        in_next = from.in_next;
+        bitbuf = from.bitbuf;
+        bitsleft = from.bitsleft;
+        overrun_count = from.overrun_count;
+        reached_final_block = from.reached_final_block;
+        return *this;
+    }
+
+    InputStream(InputStream&&) = default;
+    InputStream& operator=(InputStream&&) = default;
+
+    size_t size() const {
+        assert(in_end >= begin);
+        return in_end - begin;
+    }
+
+    /** Remaining available bytes
+     * @note align_input() should be called first in order to get accurate readings (or use available_bits() / 8)
+     */
+    size_t available() const {
+        assert(in_end >= in_next);
+        return in_end - in_next;
+    }
+
+    /// Remaining available bits
+    size_t available_bits() const {
+        return 8*available() + bitsleft;
+    }
+
+    /** Position in the stream in bytes
+     * @note align_input() should be called first in order to get accurate readings (or use position_bits() / 8)
+     */
+    size_t position() const {
+        assert(in_next >= begin);
+        return in_next - begin;
+    }
+
+    size_t position_bits() const {
+        return 8*position() - bitsleft;
+    }
+
+    /// Seek forward
+    void skip(size_t offset) {
+        align_input();
+        assert(in_next + offset < in_end);
+        in_next += offset;
+    }
+
+    /**
+     * Load more bits from the input buffer until the specified number of bits is
+     * present in the bitbuffer variable.  'n' cannot be too large; see MAX_ENSURE
+     * and CAN_ENSURE().
+     */
+    template <bitbuf_size_t n>
+    bool ensure_bits() {
+        static_assert(n <= bitbuf_max_ensure, "Bit buffer is too small");
+        if (!have_bits(n)) {
+                if(unlikely(available() < 1))
+                    return false; // This is not acceptable overrun
+                if (likely(available() >= sizeof(bitbuf_t)))
+                    fill_bits_wordwise();
+                else
+                    fill_bits_bytewise();
+        }
+        return true;
+    }
+
+    /**
+     * Return the next 'n' bits from the bitbuffer variable without removing them.
+     */
+    u32 bits(bitbuf_size_t n) const {
+        assert(bitsleft >= n);
+        return  u32(bitbuf & ((u32(1) << n) - 1));
+    }
+
+    /**
+     * Remove the next 'n' bits from the bitbuffer variable.
+     */
+    inline void remove_bits(bitbuf_size_t n) {
+        assert(bitsleft >= n);
+        bitbuf >>= n;
+        bitsleft -= n;
+    }
+
+    /**
+     * Remove and return the next 'n' bits from the bitbuffer variable.
+     */
+    inline u32 pop_bits(bitbuf_size_t n) {
+        u32 tmp = bits(n);
+        remove_bits(n);
+        return tmp;
+    }
+
+    /**
+     * Align the input to the next byte boundary, discarding any remaining bits in
+     * the current byte.
+     *
+     * Note that if the bitbuffer variable currently contains more than 8 bits, then
+     * we must rewind 'in_next', effectively putting those bits back.  Only the bits
+     * in what would be the "current" byte if we were reading one byte at a time can
+     * be actually discarded.
+     */
+    inline void align_input() {
+        assert(overrun_count <= (bitsleft >> 3));
+        in_next -= (bitsleft >> 3) - overrun_count;
+        // was:
+        //in_next -= (bitsleft >> 3) - std::min(overrun_count, bitsleft >> 3);
+        bitbuf = 0;
+        bitsleft = 0;
+    }
+
+    /**
+     * Read a 16-bit value from the input.  This must have been preceded by a call
+     * to ALIGN_INPUT(), and the caller must have already checked for overrun.
+     */
+    inline u16 pop_u16() {
+        assert(available() >= 2);
+        u16 tmp = get_unaligned_le16(in_next);
+        in_next += 2;
+        return tmp;
+    }
+
+
+    /**
+      * Copy n bytes to the ouput buffer. The input buffer must be aligned with a
+      * call to align_input()
+      */
+    inline void copy(byte* restrict out, size_t n) {
+        assert(available() >= n);
+        memcpy(out, in_next, n);
+        in_next += n;
+    }
+
+    /**
+      * Checks that the lenght next bytes are ascii
+      * (for checked copy()ies of uncompressed blocks)
+      */
+    inline bool check_ascii(size_t n) {
+        if(unlikely(n > available()))
+            return false;
+
+        for(size_t i=0 ; i < n ; i++) {
+            byte c = in_next[i];
+            if(c > byte('c') || c < byte('\t'))
+                return false;
+        }
+        return true;
+    }
+
+};
+
+#endif // INPUT_STREAM_HPP
