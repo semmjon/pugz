6ab39520079ce24dc8a08b20a34715b244e4be74
Maël Kerbiriou <m431.kerbiriou@gmail.com>
2018-05-09 00:28:28 +0200

    synchronizer: context passing between threads

--- common/synchronizer.hpp
+++ common/synchronizer.hpp
@@ -2,10 +2,13 @@
 #define SYNCHRONIZER_HPP
 
 #include <atomic>
+#include <condition_variable>
+#include <mutex>
 
 /// Keep track of where to end the deconding in the current thread
 struct alignas(64) synchronizer
 {
+    using context_ptr = unsigned char*;
 
     // Post method
     void signal_first_decoded_sequence(size_t in_pos_blk, unsigned _first_seq_block_pos)
@@ -27,9 +30,31 @@ struct alignas(64) synchronizer
         return block_pos >= first_seq_block_pos.load(std::memory_order_acquire);
     }
 
+    // Post context and wait for it to be consumed
+    void post_context(context_ptr ctx)
+    {
+        auto lock     = std::unique_lock<std::mutex>(mut);
+        this->context = ctx;
+        cond.notify_all();
+        cond.wait(lock);
+    }
+
+    template<typename F> void with_context(F&& f)
+    {
+        auto lock = std::unique_lock<std::mutex>(mut);
+        while (context == nullptr)
+            cond.wait(lock);
+        f(context);
+        cond.notify_all();
+    }
+
   protected:
-    std::atomic<size_t>   blk_start_in_pos    = {~0UL};
-    std::atomic<unsigned> first_seq_block_pos = {~0U};
+    std::mutex              mut                 = {};
+    std::condition_variable cond                = {};
+    std::atomic<size_t>     blk_start_in_pos    = {~0UL};
+    std::atomic<unsigned>   first_seq_block_pos = {~0U};
+
+    unsigned char* context = nullptr;
 };
 
 #endif // SYNCHRONIZER_HPP
